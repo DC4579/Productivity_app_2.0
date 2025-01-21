2bf3ca5cf08c83f449ff256c20b7e69affae72ed
DC <149849984+gojohatake456@users.noreply.github.com>
2025-01-21 13:27:44 +0530

    study productivity

---
+++ app/src/main/java/com/example/productivity_app/pages/studyTimer.kt
@@ -0,0 +1,24 @@
+package com.example.productivity_app.pages
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.example.productivity_app.AuthViewModel
+
+@Composable
+fun studyTimerPage(modifier: Modifier = Modifier, navController: NavController, authViewModel: AuthViewModel) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+    }
+    }
---
+++ app/src/main/java/com/example/productivity_app/pages/todoList.kt
@@ -0,0 +1,26 @@
+package com.example.productivity_app.pages
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.example.productivity_app.AuthViewModel
+
+@Composable
+fun todoListPage(modifier: Modifier = Modifier, navController: NavController, authViewModel: AuthViewModel) {
+    println("timerListPage NavController: ${System.identityHashCode(navController)}")
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+    }
+}
